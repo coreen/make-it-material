9d759a9cc1fe0664a511c18b9c0c7e2c3e2ddfe6
coreen <coreeny@cs.washington.edu>
2018-09-23 09:04:38 -0700

    Update look and feel to meet Material Design specs

--- app/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
+++ app/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
@@ -232,14 +232,18 @@ public class ArticleDetailFragment extends Fragment implements
                                 + "</font>"));
 
             }
-            bodyView.setText(Html.fromHtml(mCursor.getString(ArticleLoader.Query.BODY).replaceAll("(\r\n|\n)", "<br />")));
+            bodyView.setText(Html.fromHtml(mCursor
+                    .getString(ArticleLoader.Query.BODY)
+                    .replaceAll("(\r\n|\n)", "<br />")));
             ImageLoaderHelper.getInstance(getActivity()).getImageLoader()
                     .get(mCursor.getString(ArticleLoader.Query.PHOTO_URL), new ImageLoader.ImageListener() {
                         @Override
                         public void onResponse(ImageLoader.ImageContainer imageContainer, boolean b) {
                             Bitmap bitmap = imageContainer.getBitmap();
                             if (bitmap != null) {
-                                Palette p = Palette.generate(bitmap, 12);
+                                Palette p = new Palette.Builder(bitmap)
+                                        .maximumColorCount(12)
+                                        .generate();
                                 mMutedColor = p.getDarkMutedColor(0xFF333333);
                                 mPhotoView.setImageBitmap(imageContainer.getBitmap());
                                 mRootView.findViewById(R.id.meta_bar)
